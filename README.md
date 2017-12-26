b49cfe91842f8f932df617627f874434e3a2a009
Zigmas Bitinas <zigmbit@gmail.com>
2017-12-27 01:13:38 +0200

    New story folder structure

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-The idea of this application is to have an intuitive story-based game editor and viewer that would allow people to create stories without any programming knowledge. I hope that it inspires people to create some great stories and maybe even collaborate through github to make some massive stories.
+The idea of this application is to have an intuitive story-based game editor and viewer that would allow people to create stories without any programming knowledge. I hope that it inspires people to create some great stories and maybe even collaborate through github to make some massive stories, with extremely different plots.
 
-Currently I have only made a super basic viewer of such games that can be made using a simplistic folder structure. Even the story representation is flawed, since the paths cannot exceed 5000 characters in Linux (and even less in windows). The representation will be the first thing to change, I just wanted to save the initial "working" project. :)
+Currently I have only made a super basic viewer of games with a rather simplistic file structure. Since there currently is no editor, the only way one can write stories is by hand, following the pattern in the ExampleStory.
 
-Since there is no editor currently, the only way one can write stories is by hand, following the pattern in the ExampleStory.
-
-Once there's a working story viewer, I will start working on an editor and will try to add more functionalities.
+Once the story viewer is reasonably finished, I will start developing the editor.
--- Viewer.py
+++ Viewer.py
@@ -2,19 +2,34 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ListProperty, StringProperty
+from kivy.properties import ListProperty, StringProperty, ObjectProperty
 from kivy.uix.label import Label
 import os
 import json
-
+import types
 
 #All stories should be stored in this single directory with first character not a dot (".").
-#By default it is assumed that the script is stored in the story folder.
-#This is the directory that contains the story, not the story directory itself!!! There can be multiple stories in the same directory
-storyDir = "./"
+
+#This is the directory that contains the story. You may change it.
+storyDir = "./ExampleStory/"
+
+#For simplicity the starting screen has been removed. You have to manually add the required 
+
+# gamestate = {
+#  choices = [a, b, c, d] (ids and texts of choices?)
+#  storyText = String
+#  
+# }
+
+
+# choice 
+
 
 class RootWidget(BoxLayout):
 	storyLocation = StringProperty("./")
+	gameState = ObjectProperty({})
+	options = ObjectProperty({})
+
 	def __init__(self, **kwargs):
 		self.storyLocation=kwargs.pop("storyDir", "./")
 		if self.storyLocation[-1]!="/":
@@ -25,45 +40,35 @@ class RootWidget(BoxLayout):
 		self.storyText = Label(text="Choose a story from the list below", font_size=50)
 		self.add_widget(self.storyText)
 
+		self.options = json.load(open(self.storyLocation+"options.json"))
+		
+		self.gameState = json.load(open(self.storyLocation+"Story/"+str(self.options["startId"])+".json"))
+		self.bind(gameState=self.update)
+
 		self.buttons = BoxLayout(orientation="vertical")
 		self.add_widget(self.buttons)
 
-		self.bind(storyLocation=self.update)
+		self.update()
+	
 
-		self.updateButtons()
-
-	def update(self, *unused):
-		self.updateText()
-		self.updateButtons()
-
-	def updateButtons(self):
-		print(self.storyLocation)
-		folders = [f for f in os.listdir(self.storyLocation) if (not os.path.isfile(self.storyLocation+f))]
-		print(folders)
-		elements = list(filter(lambda s: s[0]!='.', folders))
-		print(elements)
+	def update(self, *ignore):
 		self.buttons.clear_widgets()
-		for i in elements:
-			print(i+" asdjadjka")
-			btn = Button(text=i)
+		for i in self.gameState["choices"]:
+			btn = Button(text=i["text"])
+			btn.fileId = i["id"]
 			btn.bind(on_press=self.btn_pressed)
 			self.buttons.add_widget(btn)
-		print(self.buttons)
 
-	def updateText(self):
-		gamestate=json.load(open(self.storyLocation+"gamestate.json"))
-		print(gamestate)
-		self.storyText.text=gamestate["text"]
-		self.storyText.font_size=20
+		self.storyText.text=self.gameState["storyText"]
+		self.storyText.font_size=30
+
 
 	def btn_pressed(self, instance):
-		self.storyLocation+=instance.text+"/"
-		self.update()
+		self.gameState=json.load(open(self.storyLocation+"Story/"+str(instance.fileId)+".json"))
 
 
 
 class StoryApp(App):
-
 	def build(self):
 		return RootWidget(storyDir=storyDir)
 
@@ -84,6 +89,5 @@ if __name__ == '__main__':
 # Keep a list of participating characters in a block for easier reference
 
 # TODO
-# Add custom storyLocation option
 # Fix button size
 # Experiment with different button formats
